00e3f05f1bb7ce7cee813899ccd96b512c7fd7e8
manhph-macos <manhph@manhph-macoss-MacBook-Pro.local>
2022-12-14 21:16:13 +0700

    add hw

---
+++ 2-Examples/Session 08 - Algorithm/readme.md
@@ -0,0 +1,8 @@
+– Bước 1: Phân tích số a ra thừa số nguyên tố:
+– Bước 2: Lấy số mũ của các thừa số nguyên tố a, b cộng 1 rồi nhân với nhau
+Vậy số ước của a là : (m + 1)(n + 1) trong đó (m, n là các số tự nhiên).
+Ví dụ 1: Tìm số ước của số 135
+Ta có: 135=3^3 x 5 (với 3; 5 là các số nguyên tố lần lượt có mũ là 3 và 1)
+Số ước 135 là: (3 + 1) x (1 + 1) = 4 x 2=8 (ước).
+
+Nguồn bài viết: https://toancap2.net/cach-tim-so-uoc-cua-mot-so/
--- 2-Examples/Web/js/basic.js
+++ 2-Examples/Web/js/basic.js
@@ -1,29 +1,44 @@
+// let/const <ten_bien> = <gia_tri_cua_bien>
 
+// Khai bao bien vs toan bo type da hoc
+// kieu nguyen thuy: number, string, boolean, undefined, null
+// kieu phuc tap: object, array, function
 
-let a = 4
-let b = 2
-// > < >= <= && ||
-// ===
-
-let x = 1 // number
-let y = '1'
-// cau dieu kien
-if ( (x === y) || (a>b) ) { 
-    // true
-    console.log(' a lon hon b');
-}else{
-    console.log(' b lon hon a');
+// Log console: theo format <typeof> <value>
+let num = 1;
+// console.log("number", num);
+// alert('Day la alert dong 10: ' + num)
+let number1 = prompt("Please enter your number");
+console.log(number1);
+let str = "day la string";
+console.log("string", str);
+let boo = true;
+console.log("boolean", boo);
+let und;
+let unde = undefined;
+console.log("undefined", und, unde);
+let nu = null;
+console.log("null", nu);
+let user = {
+  name: "ten user",
+  age: "tuoi user",
+};
+console.log("object", user);
+console.log("object.name", user.name);
+let ar = [1, 2, 3, 4];
+let ar1 = ["a", "b", "c"]; // -> Array<string>
+console.log("Array<number>", ar);
+console.log("lay phan tu", ar[0]); // [0] -> index : 0-> ar.length
+function getUser() {
+  let user = {
+    name: "ten user getUser",
+    age: "tuoi user getUser",
+  };
 }
-// condition rendering
-(x === y) ? console.log(' a lon hon b') : console.log(' b lon hon a')
+let res = getUser();
+console.log(res);
 
-x && console.log(1); // res of condition : boolean
-// 
-
-let z = 0
-if (z) { // false
-    console.log(true);
-}else{
-    console.log(false);
+function getUserParam(_user) {
+  console.log(_user);
 }
-
+getUserParam(user);
