a40e4cbc2b2817ed6399b4fbc41b31acc2d177a3
manhph-macos <manhph@manhph-macoss-MacBook-Pro.local>
2022-12-21 20:48:14 +0700

    update code list, detail

--- 2-Examples/Web/js/detail.js
+++ 2-Examples/Web/js/detail.js
@@ -1,14 +1,8 @@
-window.onload = function () {
-  let id = getIdUrl();
-  console.log(id);
-};
-function getIdUrl() {
-  let url = new URL(window.location.href);
-  let id = url.searchParams.get("id");
-  return id;
-}
+let urlDetail = new URLSearchParams(window.location.search);
+let id = urlDetail.get("id");
+console.log(id);
 
-const URL = "https://632878a09a053ff9aab8cf03.mockapi.io/api/v1/users/1";
+const URL = "https://632878a09a053ff9aab8cf03.mockapi.io/api/v1/users/" + id;
 
 fetch(URL, {
   method: "GET",
